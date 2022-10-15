b9ff83d062739dc2a94839ac8ecb738a187180d1
Anubhav Kushwaha <k04kushwaha@gmail.com>
2022-10-15 17:41:29 +0530

    add current song redirect url

--- spotify_banner/urls.py
+++ spotify_banner/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
 
-from .views import SpotifyRegisterView, SpotifyCallbackView, CurrentTrackView, CurrentTrackBannerView
+from .views import SpotifyRegisterView, SpotifyCallbackView, CurrentTrackBannerView, CurrentTrackRedirectView
 
 urlpatterns = [
     path("register/", SpotifyRegisterView.as_view(), name="register"),
     path("register/callback/", SpotifyCallbackView.as_view(), name="spotify-callback"),
-    path("<uuid:auth_id>/current-track/", CurrentTrackView.as_view(), name="current-track"),
+    # path("<uuid:auth_id>/current-track/", CurrentTrackView.as_view(), name="current-track"),
     path("<uuid:auth_id>/current-track/banner/", CurrentTrackBannerView.as_view(), name="current-track-banner"),
+    path("<uuid:auth_id>/current-track/redirect/", CurrentTrackRedirectView.as_view(), name="current-track-redirect"),
 ]
--- spotify_banner/views.py
+++ spotify_banner/views.py
@@ -63,5 +63,5 @@ class CurrentTrackRedirectView(SpotifyClientMixin, GenericAPIView):
     def get(self, request):
         current_track = self.client.current_user_playing_track()
         if current_track:
-            return redirect(current_track.external_urls['spotify'])
+            return redirect(current_track['item']['external_urls']['spotify'])
         return Response(status=status.HTTP_204_NO_CONTENT)
